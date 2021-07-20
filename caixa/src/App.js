3f1c8fa6b4302e748daa89622523de87c7826c1d
maikon_ola_mundo@outlook.com <maikon.muniz@yahoo.com.br>
2021-07-20 00:21:06 -0300

    version 2'

--- caixa/src/App.js
+++ caixa/src/App.js
@@ -1,29 +1,50 @@
 import React, {useState} from 'react'
+import Caixa from './componentes/caixa'
+import Operacao from './componentes/operacao'
 import './App.css';
-
 
 function App() {
 
   const [saque, setSaque] = useState(0)
   const [deposito, setDeposito] = useState(0)
-  const [base, setBase] = useState(0)
+  const [base, setBase] = useState(100000)
 
 
+  const comparaOperacao = () => {
+    if (saque == 0 && deposito == 0){
 
+      return "Nada Operado";
 
+    }
 
+    else if(saque != 0 && deposito != 0){
 
+      return "Invalido";
+
+    }
+
+    else {
+
+      return "Operando";
+
+    }
+  }
 
   return (
     <>
-    <section>
 
-    <label>Saque</label>
-        <input onChange={(a) => (setSaque(a.target.value))}/><br/>
-        <label>Deposito</label>
-        <input  onChange = {(a) => (setDeposito(a.target.value))}/>
-  
-    <p>Valor maximo do banco: {parseFloat(deposito-saque)}</p>
+    <section className="interface">
+
+        <div className="interface_div">
+          <Caixa label = "Saque" valor = {saque} setOperacao = {setSaque} />
+          <Caixa label = "Deposito" valor = {deposito} setOperacao = {setDeposito} />
+          <Operacao
+          compara = {comparaOperacao}
+          setSaque = {saque}
+          setDeposito = {deposito}
+          setBase = {parseFloat(base) + parseFloat(deposito) - parseFloat(saque)}/>
+        </div>
+
     </section>
     </>
   );
---
+++ caixa/src/componentes/operacao.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+export default function Operacao(props){
+
+
+    return(
+        <>
+
+        <p>{props.setBase}</p>
+        <p>{props.compara()}</p>
+
+        </>
+    )
+
+
+}
